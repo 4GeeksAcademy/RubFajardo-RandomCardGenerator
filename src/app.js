5dc012fd8fab26cade8f1e13d00ff48084acecd2
RubFajardo <rubenfajardo68@gmail.com>
2025-05-26 19:22:19 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let myValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', ' J' ,'Q', 'K'];
+let suits = ['♦', '♥', '♠', '♣'];
+
+let randomSuit = suits[Math.floor(Math.random() * 4)]
+let randomValue = myValues[Math.floor(Math.random() * 12)]
+
+let myTop = document.querySelector('#top');
+let myValue = document.querySelector('#value');
+let myBottom = document.querySelector('#bottom');
+
+function randomizer () {
+  if (randomSuit === '♥' || randomSuit === '♦') {
+    myTop.style.color = 'red'
+    myBottom.style.color = 'red'
+    myTop.innerHTML = randomSuit
+    myBottom.innerHTML = randomSuit
+  }
+  if (randomValue === '10') {
+    myValue.style.top = '26%'
+    myValue.style.left = '25%'
+    myValue.innerHTML = randomValue
+  }
+  myTop.innerHTML = randomSuit
+  myBottom.innerHTML = randomSuit
+  myValue.innerHTML = randomValue
+}
+
+window.onload = randomizer();
